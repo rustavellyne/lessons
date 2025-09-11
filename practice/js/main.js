767fb02cdb76b0311c96c0aa378e72ae6998a7cc
Alex Cherniakov <rustavellyne@gmail.com>
2025-09-11 12:57:03 +0300

    hero banners finished

--- practice/js/main.js
+++ practice/js/main.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function() {
   const hamburger = document.querySelector('.burger-btn');
   hamburger.addEventListener('click', e => {
     hamburger.classList.toggle('opened');
-    document.documentElement.classList.toggle('menu-open')
+    document.documentElement.classList.toggle('menu-open');
+    document.body.classList.toggle('no-scroll');
   })
 });
