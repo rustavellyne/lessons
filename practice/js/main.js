371b4af13b59fd0649dc995017c46f05bfe971de
Alex Cherniakov <rustavellyne@gmail.com>
2025-09-20 18:02:13 +0300

    testimonials slider finish

--- practice/js/main.js
+++ practice/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function() {
+  initSlider();
   const hamburger = document.querySelector('.burger-btn');
   hamburger.addEventListener('click', e => {
     hamburger.classList.toggle('opened');
@@ -6,3 +7,39 @@ window.addEventListener('load', function() {
     document.body.classList.toggle('no-scroll');
   })
 });
+
+function initSlider() {
+  const sliderEl = document.querySelector('.testimonials-slider');
+  if (sliderEl) {
+    const swiper = new Swiper('.testimonials-slider', {
+      loop: true,
+
+      navigation: {
+        nextEl: '.testimonials-button__next',
+        prevEl: '.testimonials-button__prev',
+      },
+      breakpoints: {
+        320: {
+          spaceBetween: 10,
+          slidesPerView: 1.1,
+        },
+        640: {
+          slidesPerView: 1.5,
+          spaceBetween: 14,
+        },
+        768: {
+          slidesPerView: 2.1,
+          spaceBetween: 18,
+        },
+        924: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+        1150: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        }
+      },
+    });
+  }
+}
